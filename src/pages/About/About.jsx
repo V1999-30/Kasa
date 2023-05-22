b5538e2535c109b07a79484e5491d7eca6d6e5d0
V1999-30 <berzosa.valentin@gmail.com>
2023-05-22 09:21:45 +0200

    Modif Banner, Collapse, Footer, About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -15,7 +15,7 @@ function About () {
                 <Menu image={menuLogo}/>
             </div>
             <div className="About">
-                <Banner image={bannerAbout} texte="" hauteur="223px"/>
+                <Banner image={bannerAbout} texte="" hauteur="300px"/>
                 <Collapse titre="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."/>
                 <Collapse titre="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
                 <Collapse titre="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
